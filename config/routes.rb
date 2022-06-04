63185abde82ef63dd7ca713edab43a00ee1ccbc8
Nurina Junus <nurina.junus@gmail.com>
2022-06-04 16:18:10 -0600

    update fetch works

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get "/me", to: "users#show"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    patch "/hikes/:id/reviews", to: "reviews#update"
     resources :reviews
     resources :hikes, only: [:index, :show, :update] do
       resources :reviews, shallow: true
